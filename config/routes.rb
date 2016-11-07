e4367a11c32ab4642333d5072a43cee100c25648
litenup <alanhsu@litenvoice.com>
2016-11-07 15:17:51 -0800

    Join routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ UserImpersonate::Engine.routes.draw do
   get "/user/:user_id" , to: "impersonate#create", as: :impersonate_user
   delete "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
 
-  root to: "impersonate#index"
+  get "/impersonate", to: "impersonate#index"
 end
--- lib/user_impersonate/engine.rb
+++ lib/user_impersonate/engine.rb
@@ -1,17 +1,11 @@
 module UserImpersonate
   class Engine < ::Rails::Engine
-    isolate_namespace UserImpersonate
+    # isolate_namespace UserImpersonate
     
     initializer "user_impersonate.devise_helpers" do
       if Object.const_defined?("Devise")
         require "user_impersonate/devise_helpers"
         Devise.include_helpers(UserImpersonate::DeviseHelpers)
-      end
-    end
-
-    initializer 'user_impersonate.action_controller' do |app|
-      ActiveSupport.on_load :action_controller do
-        helper UserImpersonate::ApplicationHelper
       end
     end
 
